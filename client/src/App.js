fac4d355bd15bb11615327cadf386c1653f3a666
Mohamed Ashref Ben Abdallah <mohamedashrefbenabdallah@gmail.com>
2022-08-27 14:32:07 +0100

    tweaked the footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,34 @@
 import React, { useEffect } from 'react';
-import {
-  ChakraProvider,
-  theme,
-} from '@chakra-ui/react';
+import { ChakraProvider, theme } from '@chakra-ui/react';
 import { Main } from './pages/Main';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIn from './pages/Login/SignIn';
-import SignupCard  from './pages/sign up/Signup';
+import SignupCard from './pages/sign up/Signup';
 import PageProjects from './pages/pageProjects';
 import Simple from './pages/project';
 import axios from 'axios';
 //ena ashref
 
 function App() {
-
   useEffect(() => {
     return () => {
       axios.create({
-        baseURL: 'http://localhost:5000'
-      })
-    }
-  }, [])
-  
-  
+        baseURL: 'http://localhost:5000',
+      });
+    };
+  }, []);
+
   return (
     <ChakraProvider theme={theme}>
-<Router>
-      <Routes> 
-      <Route path='/' exact element={ <Main/>} />
-      <Route path='/signin' element={<SignIn/>} />
-      <Route path='/signup' element={<SignupCard/>} />
-      <Route path='/projects/:id' element={<PageProjects />} />
-      <Route path='/project_details/:id' element={<Simple />} />
-
- 
-</Routes>
-     
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Main />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignupCard />} />
+          <Route path="/projects/:id" element={<PageProjects />} />
+          <Route path="/project_details/:id" element={<Simple />} />
+        </Routes>
       </Router>
-
     </ChakraProvider>
   );
 }
